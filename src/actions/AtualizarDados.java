70e068e04d77b714e90c5caccbda43d10c755839
gbsantstech <contatogbsants@outlook.com>
2022-12-02 13:58:50 -0300

    Alteração feita em Atualizar dados

--- src/actions/AtualizarDados.java
+++ src/actions/AtualizarDados.java
@@ -23,12 +23,23 @@ public class AtualizarDados {
 		String id = scan.next();
 	
 		aluno = manager.find(Aluno.class, Long.parseLong(id));
-		System.out.println("\nNome: ");
-		String nome = scan.nextLine();
 
 		
 		manager.getTransaction().begin();
+		
+		System.out.println("\nNome: ");
+		scan.nextLine();
 		aluno.setNome(scan.nextLine());
+		System.out.println("\nE-mail: ");
+		aluno.setEmail(scan.nextLine());
+		System.out.println("\nCPF: ");
+		aluno.setCpf(scan.nextLine());
+		System.out.println("\nData de Nascimento: ");
+		aluno.setDataNasc(scan.nextLine());
+		System.out.println("\nNaturalidade: ");
+		aluno.setNaturalidade(scan.nextLine());
+		System.out.println("\nEndereço completo: ");
+		aluno.setEndereco(scan.nextLine());
 		manager.getTransaction().commit();
 		
 		System.out.println("Atualizado com sucesso!");
